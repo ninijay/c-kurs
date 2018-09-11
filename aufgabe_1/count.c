d3a0e2f0cadf58e52ffba8b9fbb2c4eac41da957
Mattia Ninivaggi <ninijay93@gmail.com>
2018-09-11 15:49:43 +0200

    fixed bug

--- aufgabe_1/count.c
+++ aufgabe_1/count.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+
 
 int main(void){
 	int cnt = 0;
-	int cnt_words = 1;
+	int cnt_words = 0;
 	int c;
+	int hasWord = FALSE;
 	while((c = getchar()) != '\n' && c != EOF){
 		cnt++;
-		if(c == ' ' || c == '\t')
-		{
+		if(!hasWord && c != ' ' &&  c != '\t'){
+			hasWord = TRUE;
 			cnt_words++;
 		}
+		if((c == ' ' || c == '\t') && hasWord)
+			cnt_words++;
 	}
 
 	(void)printf("Anzahl Zeichen %7d\n", cnt);
